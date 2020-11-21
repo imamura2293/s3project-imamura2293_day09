c091f668b4bc0b7521facc0704ccc4a48a6d5f2c
imamura2293 <uu77ee22dd55>
2020-11-21 15:29:50 +0900

    2html

--- main.py
+++ main.py
@@ -13,5 +13,15 @@ def home():
     return 'homeだよ～'
 
 
+@app.route('/greet/<string:name>')
+def greet(name: str):
+    return f'Hello! {name}'
+
+
+@app.route('/shout/<string:name>')
+def shout(name: str):
+    return f'{name.upper()}!!!'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
